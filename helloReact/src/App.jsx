31145369e394713167ccc536f7685b8d7c94425a
Jouusey <Josevitor110m.p@gmail.com>
2024-08-19 17:07:49 -0300

    Adicionado hello React(E CORES!)

---
+++ helloReact/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+ 
+ <h1 className='titulo1'>Hello React JS.</h1>
+
+<p className='paragrafo'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+  Id nostrum blanditiis molestias quas quod porro tempora beatae,
+  consequuntur fugit voluptates eaque labore optio quo expedita sit hic.
+  Ipsum, ipsam necessitatibus?</p>
+
+    </>
+  )
+
+}
+
+export default App
