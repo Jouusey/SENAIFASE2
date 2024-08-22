b890731bba5bc9e0e3ec9160df3524e8cb9ce274
Jouusey <Josevitor110m.p@gmail.com>
2024-08-22 16:58:30 -0300

    Criação

---
+++ BancoDados/src/components/Body.jsx
@@ -0,0 +1,10 @@
+import './Body.css'
+function Body() {
+  return (
+    <div className="Body-Container">
+      <h2 className="Body-Texto">22/08/2024</h2>
+    </div>
+  )
+}
+
+export default Body
---
+++ BancoDados/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import './Header.css'
+
+
+
+
+function Header() {
+  return (
+    <div className='Header-Container'>
+      
+      <h1 className='Header-titulo'>BANCO DE DADOS</h1>
+      <button className='btn'>BANCAR</button>
+
+    </div>
+  )
+}
+
+export default Header
