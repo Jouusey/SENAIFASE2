4234e097f81051f14ed2fb8937f2809b6ea653cb
Jouusey <Josevitor110m.p@gmail.com>
2024-09-11 17:11:19 -0300

    Começo da atividade de render e inputs

---
+++ ExercicioRenderInput/src/App.jsx
@@ -0,0 +1,50 @@
+import './App.css'
+function App() {
+
+
+
+
+
+
+
+
+
+
+  return (
+    <>
+    <div className="exercicio-container">
+
+
+        <div className="exercicio-esquerda">Lorem ipsum dolor sit
+         amet consectetur adipisicing elit. Cupiditate dolo
+         re molestiae sapiente enim cumque eos, magnam vi
+        tae ea provident. Reprehenderit ipsa incidunt, fugi
+        at quam tempore nobis! Tempora iusto tempore vitae!</div>
+        
+
+
+        <div className="exercicio-direita">
+
+              <div className="exercicio-coisas">Lorem ipsum d
+              olor sit amet consectetur adipisicing elit. Tempora s
+              unt quisquam autem beatae dolore culpa, ducimus dolorem nem
+              o minima, a illo enim corporis aspernatur amet distinc
+              tio impedit perferendis, quos iure?</div>
+              <div className="exercicio-outrasCoisas">Lorem ipsum
+               dolor sit amet consectetur adipisicing elit. Quam qu
+               o magni impedit ipsa sed fuga ullam ipsam. Ea nostrum eum quae
+                quasi rem! Odio maiores assumenda, distinctio officiis corporis 
+                nostrum?</div>
+
+        </div>
+      
+
+
+
+
+    </div>
+    </>
+  )
+}
+
+export default App
